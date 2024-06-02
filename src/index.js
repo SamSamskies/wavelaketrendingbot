e389b1364a4ff8e73455c23418bb96538dc5fb2c
Sam Samskies <samsamskies@gmail.com>
2024-06-02 15:55:40 -0500

    1st draft

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const { useWebSocketImplementation, Relay } = require("nostr-tools/relay");
+const { npubEncode } = require("nostr-tools/nip19");
+
+useWebSocketImplementation(require("ws"));
+
+const start = async () => {
+  const relayUri = "wss://relay.wavlake.com";
+  const relay = await Relay.connect(relayUri);
+  const events = [];
+  const numberOfEvents = 10;
+
+  const sub = relay.subscribe(
+    [
+      {
+        kinds: [9735],
+        since: Math.floor(Date.now() / 1000) - 24 * 60 * 60, // last 24 hours
+        "#p": [
+          "7759fb24cec56fc57550754ca8f6d2c60183da2537c8f38108fdf283b20a0e58",
+        ],
+      },
+    ],
+    {
+      onevent(event) {
+        if (event.tags.find((t) => t[0] === "a")) {
+          events.push(
+            JSON.parse(event.tags.find((t) => t[0] === "description")[1]),
+          );
+        }
+      },
+      oneose() {
+        const getAmount = (event) =>
+          Number(event.tags.find((t) => t[0] === "amount")[1]);
+
+        sub.close();
+        relay.close();
+        events.sort((a, b) => getAmount(a) - getAmount(b));
+
+        events.slice(-numberOfEvents).forEach((event) => {
+          const zapperNpub = npubEncode(event.pubkey);
+          const zapAmount = getAmount(event) / 1000;
+          const comment =
+            event.content.length === 0
+              ? event.content
+              : `"${event.content}"\n\n`;
+          const trackId = event.tags.find((t) => t[0] === "a")[1].split(":")[2];
+          const trackLink = `https://wavlake.com/track/${trackId}`;
+          console.log(
+            `nostr:${zapperNpub} zapped ⚡️${zapAmount.toLocaleString()} sats\n\n${comment}${trackLink}\n\n`,
+          );
+        });
+      },
+    },
+  );
+};
+
+try {
+  start();
+} catch (err) {
+  console.error(err);
+}
